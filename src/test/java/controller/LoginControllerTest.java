0964ec45069944e2cd5f7c1a270d25762aca7a32
Enrico Pasquali <calcifer.code@gmail.com>
2019-01-28 21:45:27 +0100

    Add loginControllerTest

---
+++ src/test/java/controller/LoginControllerTest.java
@@ -0,0 +1,33 @@
+package controller;
+
+import model.UserModel;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+    private String validEmail = "mail@mail.com";
+    private String validPassword = "qwerty";
+    private String validRole = "Magazziniere";
+    private UserModel referenceUserModel;
+
+    @Test
+    void loginSuccessTest() {
+        referenceUserModel = new UserModel(validEmail, validRole);
+        LoginController loginController = new LoginController();
+        UserModel validUserModel = loginController.login(validEmail, validPassword);
+
+        assertNotNull(validUserModel);
+        assertEquals(referenceUserModel.getEmail(), validUserModel.getEmail());
+        assertEquals(referenceUserModel.getRole(), validUserModel.getRole());
+        assertEquals(validEmail, validUserModel.getEmail());
+        assertEquals(validRole, validUserModel.getRole());
+    }
+
+    @Test
+    void loginFailTest() {
+        LoginController loginController = new LoginController();
+        UserModel invalidUserModel = loginController.login("invalid@mail.com", "invalidPassword");
+
+        assertNull(invalidUserModel);
+    }
+}
