9fd5213ae1247ead5f59f6c34a4cd453c5a91aa5
Enrico Pasquali <calcifer.code@gmail.com>
2019-01-28 23:17:35 +0100

    Add Position model

---
+++ src/main/java/model/PositionModel.java
@@ -0,0 +1,13 @@
+package model;
+
+public class PositionModel {
+    private String pos;
+
+    public PositionModel(String pos) {
+        this.pos = pos;
+    }
+
+    public String getRawPosition() {
+        return pos;
+    }
+}
