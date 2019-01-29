3b41b4394b85b12339121769aa8dfce94ce67979
Enrico Pasquali <calcifer.code@gmail.com>
2019-01-29 13:33:51 +0100

    Add Sport model

---
+++ src/main/java/model/SportModel.java
@@ -0,0 +1,17 @@
+package model;
+
+public class SportModel {
+    private String name;
+
+    public SportModel(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
