57ce109fbbf126cdfbab67597d59b222638721b0
Enrico Pasquali <calcifer.code@gmail.com>
2019-01-29 16:05:24 +0100

    Add moveArticlePositionByCodes() and update()

--- src/main/java/controller/MoveArticleController.java
+++ src/main/java/controller/MoveArticleController.java
@@ -1,13 +1,17 @@
 package controller;
 
+import database.DatabaseWrapper;
 import model.PositionModel;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class MoveArticleController {
-    PositionController positionController;
-    ArticleController articleController;
+    private PositionController positionController;
+    private ArticleController articleController;
 
     public MoveArticleController() {
         positionController = new PositionController();
@@ -24,6 +28,39 @@ public class MoveArticleController {
 
     public DefaultComboBoxModel<String> getArticleCodesComboBoxModel() {
         return new DefaultComboBoxModel<>(articleController.getArticlesCodes());
+    }
+
+    public boolean moveArticlePositionByCodes(String articleCode, String positionId) {
+        DatabaseWrapper db = new DatabaseWrapper();
+        Connection con = db.getCon();
+        PreparedStatement updateArticleStmt;
+        int affectedRows = 0;
+
+        String updateArticleQuery = "UPDATE articolo SET posizione = ? WHERE codice LIKE ?";
+
+        try {
+            updateArticleStmt = con.prepareStatement(updateArticleQuery);
+            updateArticleStmt.setString(1, positionId);
+            updateArticleStmt.setString(2, articleCode);
+            affectedRows = updateArticleStmt.executeUpdate();
+            con.commit();
+        } catch (SQLException e) {
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            db.close();
+        }
+
+        return affectedRows != 0;
+    }
+
+    public void update() {
+        articleController.update();
+        positionController.update();
     }
 
     private String[] positionModelsToStrings(List<PositionModel> positionModels) {
