50d5d9538af606905439e49dbbc6b17694952f46
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-04 17:41:30 +0100

    Add equals(), hashCode()

--- src/main/java/model/PositionModel.java
+++ src/main/java/model/PositionModel.java
@@ -10,4 +10,24 @@ public class PositionModel {
     public String getRawPosition() {
         return pos;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (! (o instanceof  PositionModel)) {
+            return false;
+        }
+
+        PositionModel article = (PositionModel)o;
+
+        return pos.equals(article.pos);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+
+        result = 31 * result + pos.hashCode();
+
+        return result;
+    }
 }
