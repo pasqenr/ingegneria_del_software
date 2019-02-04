4d1122d1df44df44dd7fa7b45e52647b5f3a757c
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-04 17:50:21 +0100

    Add InsertEntranceController test

---
+++ src/test/java/controller/InsertEntranceControllerTest.java
@@ -0,0 +1,58 @@
+package controller;
+
+import model.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertEntranceControllerTest {
+
+    @Test
+    void insertArticlesAsEntranceTest() {
+        ArticleTypeController articleTypeController = new ArticleTypeController();
+        PositionController positionController = new PositionController();
+        InsertEntranceController insertEntranceController = new InsertEntranceController();
+
+        String[] articleCodes = new String[] {"AC8528090", "AC8528091"};
+        String[] articleTypes = new String[] {"Materiale Acquagym", "Materiale Acquagym"};
+        String[] articlePositions = new String[] {"A010106", "A010107"};
+        String[] articlePrices = new String[] {"12.99", "15.99"};
+        String[] articleProductionDates = new String[] {"2014-01-30", "2014-02-16"};
+        List<ArticleModel> articles = new ArrayList<>();
+
+        for (int i = 0; i < articleCodes.length; i++) {
+            ArticleType articleType = articleTypeController.findArticleTypeByName(articleTypes[i]);
+            PositionModel position = positionController.findPositionByCode(articlePositions[i]);
+
+            assertNotNull(articleType);
+            assertNotNull(position);
+
+            articles.add(new ArticleModel(articleCodes[i],
+                    articleType,
+                    articlePrices[i],
+                    articleProductionDates[i],
+                    position));
+        }
+
+        insertEntranceController.insertArticlesAsEntrance(articles);
+        ArticleController articleController = new ArticleController();
+
+        ArticleModel article1 = articleController.getArticleByCode(articleCodes[0]);
+        ArticleModel article2 = articleController.getArticleByCode(articleCodes[1]);
+
+        assertNotNull(article1);
+        assertNotNull(article2);
+        assertEquals(article1.getPosition().getRawPosition(), articlePositions[0]);
+        assertEquals(article2.getPosition().getRawPosition(), articlePositions[1]);
+
+        EntranceModel entrance = EntranceController.getEntranceByCode(EntranceModel.getGreatestCode());
+
+        assertEquals(EntranceModel.getGreatestCode(), 2);
+        assertNotNull(entrance);
+        assertEquals(entrance.getCode(), 2);
+
+    }
+}
