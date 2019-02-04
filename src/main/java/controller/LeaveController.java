c0fae7d5f677444f39ede1f381ad980c5f04e8d4
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-04 23:05:43 +0100

    Add more fields and remove old rawQuery()

--- src/main/java/controller/LeaveController.java
+++ src/main/java/controller/LeaveController.java
@@ -3,7 +3,10 @@ package controller;
 import database.DatabaseWrapper;
 import model.LeaveOrdersModel;
 import model.OrdersModel;
+
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LeaveController {
     private LeaveOrdersModel ordersModel;
@@ -18,20 +21,30 @@ public class LeaveController {
     }
 
     static public ResultSet fetchEntranceOrders(DatabaseWrapper db) {
-        ResultSet rs;
+        ResultSet rs = null;
 
         String query =
                 "SELECT o.numero_bolla, " +
                         "o.data AS data_ordine, " +
+                        "n.nome AS negozio, " +
+                        "o.spedizioniere, " +
                         "a.codice AS codice_articolo, " +
                         "ta.nome, a.prezzo " +
                         "FROM tipo_articolo AS ta " +
                         "JOIN articolo a on ta.nome = a.tipo_articolo " +
                         "JOIN ordine_articolo oa on a.codice = oa.codice_articolo " +
                         "JOIN ordine o on oa.numero_bolla = o.numero_bolla " +
+                        "JOIN negozio n on o.negozio = n.nome " +
                         "ORDER BY o.numero_bolla";
 
-        rs = db.rawQuery(query);
+        PreparedStatement stmt;
+
+        try {
+            stmt = db.getCon().prepareStatement(query);
+            rs = stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return rs;
     }
