aa0a85af1e329c7ba4d512554019a99419f48ea2
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-04 17:48:22 +0100

    Add abstract Controller
    Should be done better

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,9 @@
+package controller;
+
+public abstract class Controller {
+
+    /**
+     * Update the internal data structures to match the database data
+     */
+    abstract public  void update();
+}
