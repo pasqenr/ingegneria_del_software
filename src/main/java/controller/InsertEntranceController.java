8ca6a9a09810f5d2ff744c300e866c3426279a9d
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-04 17:50:04 +0100

    Add InsertEntranceController

---
+++ src/main/java/controller/InsertEntranceController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import model.ArticleModel;
+import model.EntranceArticleModel;
+import model.EntranceModel;
+
+import java.util.List;
+
+public class InsertEntranceController extends Controller {
+    private EntranceArticleModel entranceArticleModel;
+
+    public InsertEntranceController() {}
+
+    public void insertArticlesAsEntrance(List<ArticleModel> articles) {
+        // Create a new entrance
+        EntranceModel entrance = createNewEntrance();
+
+        // Insert the new articles
+        articles.forEach(ArticleModel::store);
+
+        // Insert article entrance
+        entranceArticleModel = new EntranceArticleModel(articles, entrance);
+        entranceArticleModel.store();
+    }
+
+    private EntranceModel createNewEntrance() {
+        insertEntrance();
+
+        return fetchLastEntrance();
+    }
+
+    private void insertEntrance() {
+        EntranceModel entrance = new EntranceModel();
+        entrance.store();
+    }
+
+    private EntranceModel fetchLastEntrance() {
+        int greatestCode = EntranceModel.getGreatestCode();
+
+        return EntranceController.getEntranceByCode(greatestCode);
+    }
+
+    @Override
+    public void update() {
+        System.err.println("Update function non implemented in " + this.getClass().getSimpleName());
+    }
+}
