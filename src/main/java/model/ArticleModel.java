aa0b168a4bb109501913b166a03a6e353a234d4e
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-04 17:40:47 +0100

    Add to ArticleModel: store(), equals(), hashCode(), compareTo()

--- src/main/java/model/ArticleModel.java
+++ src/main/java/model/ArticleModel.java
@@ -1,6 +1,11 @@
 package model;
 
-public class ArticleModel {
+import database.DatabaseWrapper;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ArticleModel extends Model implements Comparable {
     private String code;
     private ArticleType articleType;
     private String price;
@@ -56,11 +61,77 @@ public class ArticleModel {
     }
 
     @Override
+    public boolean store() {
+        DatabaseWrapper db = new DatabaseWrapper();
+        String insertQuery = "INSERT INTO articolo (codice, tipo_articolo, prezzo, data_produzione, posizione) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement stmt;
+        boolean result = false;
+
+        try {
+            stmt = db.getCon().prepareStatement(insertQuery);
+            stmt.setString(1, code);
+            stmt.setString(2, articleType.getName());
+            stmt.setString(3, price);
+            stmt.setString(4, productionDate);
+            stmt.setString(5, position.getRawPosition());
+            result = stmt.execute();
+            db.getCon().commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                db.getCon().rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        db.close();
+
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "ArticleModel. code: " + code +
                 ", articleType: " + articleType.getName() +
                 ", price: " + price +
                 ", produtionDate: " + productionDate +
                 ", position: " + position.getRawPosition();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (! (o instanceof  ArticleModel)) {
+            return false;
+        }
+
+        ArticleModel article = (ArticleModel)o;
+
+        return code.equals(article.code) &&
+                articleType.equals(article.articleType) &&
+                price.equals(article.price) &&
+                productionDate.equals(article.productionDate) &&
+                position.equals(article.position);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+
+        result = 31 * result + code.hashCode();
+        result = 31 * result + articleType.hashCode();
+        result = 31 * result + price.hashCode();
+        result = 31 * result + productionDate.hashCode();
+        result = 31 * result + position.hashCode();
+
+        return result;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        ArticleModel articleModel = (ArticleModel)o;
+
+        return code.compareTo(articleModel.code);
     }
 }
