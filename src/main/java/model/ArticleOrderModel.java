66640d0d67bd2912ee2c6b8dfd856459227b696a
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-05 15:06:51 +0100

    Add ArticleOrder model

---
+++ src/main/java/model/ArticleOrderModel.java
@@ -0,0 +1,34 @@
+package model;
+
+import database.DatabaseWrapper;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ArticleOrderModel extends Model {
+    public static void storeAll(List<ArticleModel> articles, int orderNumber) {
+        DatabaseWrapper db = new DatabaseWrapper();
+        String query = "INSERT INTO ordine_articolo (codice_articolo, numero_bolla) VALUES (?, ?)";
+        PreparedStatement stmt;
+
+        for (ArticleModel article : articles) {
+            try {
+                stmt = db.getCon().prepareStatement(query);
+                stmt.setString(1, article.getCode());
+                stmt.setInt(2, orderNumber);
+                stmt.execute();
+                db.getCon().commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        db.close();
+    }
+
+    @Override
+    public boolean store() {
+        return false;
+    }
+}
