f2872292350c6484f0667f986cab8555bc8cc413
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-06 23:52:57 +0100

    Add Javadoc for database.

--- src/main/java/database/DatabaseWrapper.java
+++ src/main/java/database/DatabaseWrapper.java
@@ -2,27 +2,48 @@ package database;
 
 import java.sql.*;
 
+/**
+ * A Database wrapper for the underling database jdbc connection.
+ */
 public class DatabaseWrapper implements AutoCloseable {
     private Connection conn = null;
 
-    public DatabaseWrapper(String url) {
+    /**
+     * Create a new connection to the url.
+     *
+     * @param url The url of the database.
+     * @param autoCommit <code>true</code> to enable the autoCommit, <code>false</code> otherwise.
+     */
+    public DatabaseWrapper(String url, boolean autoCommit) {
         connect(url);
 
         try {
-            conn.setAutoCommit(false);
+            conn.setAutoCommit(autoCommit);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Create a new connection to the standard database.
+     */
     public DatabaseWrapper() {
-        this("jdbc:sqlite:" + System.getProperty("user.dir") + "/src/main/resources/magazzino.sqlite");
+        this("jdbc:sqlite:" + System.getProperty("user.dir") + "/src/main/resources/magazzino.sqlite",
+                false);
     }
 
+    /**
+     * @return Return the underling Connection.
+     */
     public Connection getCon() {
         return conn;
     }
 
+    /**
+     * Connect to the url provided.
+     *
+     * @param url The url of the database.
+     */
     private void connect(String url) {
         try {
             conn = DriverManager.getConnection(url);
@@ -31,6 +52,12 @@ public class DatabaseWrapper implements AutoCloseable {
         }
     }
 
+    /**
+     * Send the query to the database.
+     *
+     * @param query The query to send.
+     * @return The ResultSet.
+     */
     public ResultSet rawQuery(String query) {
         Statement stmt;
         ResultSet rs = null;
@@ -45,6 +72,9 @@ public class DatabaseWrapper implements AutoCloseable {
         return rs;
     }
 
+    /**
+     * Close the connection and free resources.
+     */
     @Override
     public void close() {
         try {
@@ -58,6 +88,9 @@ public class DatabaseWrapper implements AutoCloseable {
         conn = null;
     }
 
+    /**
+     * Commit the current transaction.
+     */
     public void commit() {
         try {
             conn.commit();
