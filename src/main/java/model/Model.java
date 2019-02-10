3a452f222ee96185ff486fcec9902e53713a8e61
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-10 14:25:54 +0100

    Remove store() because is in GenericDAO interface

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,13 +1,4 @@
 package model;
 
-/**
- * A generic Model.
- */
 public abstract class Model {
-    /**
-     * Save in the database the current state of the object, creating a new entry or more entries.
-     *
-     * @return <code>true</code> if the object was stored, <code>false</code> otherwise.
-     */
-    public abstract boolean store();
 }
