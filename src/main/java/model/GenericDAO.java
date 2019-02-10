e7aa2caf2591fb1334863b7fdb1612ce0b582ba4
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-10 14:25:28 +0100

    Add GenericDAO interface to use DAO pattern

---
+++ src/main/java/model/GenericDAO.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.List;
+
+/**
+ * GenericDAO interface.
+ */
+public interface GenericDAO {
+    /**
+     * Returns the Model identified by the id.
+     *
+     * @param id A valid identifier.
+     * @return The Model identified by the id.
+     */
+    Model find(String id);
+
+    /**
+     * Returns all the Models.
+     *
+     * @return A list of all the Models in the database.
+     */
+    List<? extends Model> findAll();
+
+    /**
+     * Save in the database the current state of the object, creating a new entry or more entries.
+     *
+     * @return <code>true</code> if the object was stored, <code>false</code> otherwise.
+     */
+    boolean store();
+}
