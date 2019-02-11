fc884fc7f7dcda95044c04d57f6dbfc800e6f71c
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-11 21:38:14 +0100

    Add getInstance() and implement findAll()

--- src/main/java/model/PositionModel.java
+++ src/main/java/model/PositionModel.java
@@ -5,13 +5,14 @@ import database.DatabaseWrapper;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Represent a Position, table <code>posizione</code>.
  */
 public class PositionModel extends Model implements GenericDAO {
-    private String code;
+    private final String code;
 
     /**
      * Create a new Position.
@@ -27,6 +28,10 @@ public class PositionModel extends Model implements GenericDAO {
      */
     public String getCode() {
         return code;
+    }
+
+    public static PositionModel getInstance() {
+        return new PositionModel(null);
     }
 
     @Override
@@ -62,7 +67,7 @@ public class PositionModel extends Model implements GenericDAO {
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
-                position = new PositionModel(rs.getString("id_posizione"));
+                position = buildSingleFromResult(rs);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -76,11 +81,48 @@ public class PositionModel extends Model implements GenericDAO {
 
     @Override
     public List<PositionModel> findAll() {
-        return null;
+        List<PositionModel> positions = new ArrayList<>();
+        DatabaseWrapper db = new DatabaseWrapper();
+        String query = "SELECT a.codice, a.tipo_articolo, a.prezzo, a.data_produzione, a.posizione FROM articolo a";
+        PreparedStatement stmt;
+
+        try {
+            stmt = db.getCon().prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                PositionModel article = buildSingleFromResult(rs);
+                positions.add(article);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        db.close();
+
+        return positions;
     }
 
     @Override
     public boolean store() {
         return false;
+    }
+
+    /**
+     * Create only one Position using the data found in rs. The cursor is moved forward.
+     *
+     * @param rs The ResultSet containing the Position or Positions fetched from the database.
+     * @return A new Positon.
+     */
+    private PositionModel buildSingleFromResult(ResultSet rs) {
+        String code = null;
+
+        try {
+            code = rs.getString("id_posizione");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return new PositionModel(code);
     }
 }
