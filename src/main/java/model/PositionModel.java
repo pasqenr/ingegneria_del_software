0c5f5c67805bca73f05de0bdc18d8c88c490b8da
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-11 21:25:56 +0100

    Implement find() and change 'pos' to 'code'

--- src/main/java/model/PositionModel.java
+++ src/main/java/model/PositionModel.java
@@ -1,27 +1,32 @@
 package model;
 
+import database.DatabaseWrapper;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Represent a Position, table <code>posizione</code>.
  */
 public class PositionModel extends Model implements GenericDAO {
-    private String pos;
+    private String code;
 
     /**
      * Create a new Position.
      *
-     * @param pos A valid Position code.
+     * @param code A valid Position code.
      */
-    public PositionModel(String pos) {
-        this.pos = pos;
+    public PositionModel(String code) {
+        this.code = code;
     }
 
     /**
      * @return The String representation of the Position, that is, the Position identifier.
      */
     public String getRawPosition() {
-        return pos;
+        return code;
     }
 
     @Override
@@ -32,21 +37,41 @@ public class PositionModel extends Model implements GenericDAO {
 
         PositionModel position = (PositionModel)o;
 
-        return pos.equals(position.pos);
+        return code.equals(position.code);
     }
 
     @Override
     public int hashCode() {
         int result = 17;
 
-        result = 31 * result + pos.hashCode();
+        result = 31 * result + code.hashCode();
 
         return result;
     }
 
     @Override
-    public PositionModel find(String id) {
-        return null;
+    public PositionModel find(String code) {
+        PositionModel position = null;
+        DatabaseWrapper db = new DatabaseWrapper();
+        String query = "SELECT p.id_posizione FROM posizione p WHERE p.id_posizione = ?";
+        PreparedStatement stmt;
+
+        try {
+            stmt = db.getCon().prepareStatement(query);
+            stmt.setString(1, code);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                position = new PositionModel(rs.getString("id_posizione"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        db.close();
+
+        return position;
     }
 
     @Override
