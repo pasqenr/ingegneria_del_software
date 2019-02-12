4fe7b8598c3d6a4e568c1c360542bdfb904c7cd3
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-12 21:05:30 +0100

    Add checkEqualLengthsTest() and checkIsAlreadyStoredArticleCodeTest()

--- src/test/java/controller/InsertEntranceControllerTest.java
+++ src/test/java/controller/InsertEntranceControllerTest.java
@@ -12,14 +12,14 @@ class InsertEntranceControllerTest extends GenericControllerTest {
 
     @Test
     void insertArticlesAsEntranceTest() {
-        PositionController positionController = new PositionController();
-        InsertEntranceController insertEntranceController = new InsertEntranceController();
+        final PositionController positionController = new PositionController();
+        final InsertEntranceController insertEntranceController = new InsertEntranceController();
 
-        String[] articleCodes = new String[] {"AC8528090", "AC8528091"};
-        String[] articleTypes = new String[] {"Materiale Acquagym", "Materiale Acquagym"};
-        String[] articlePositions = new String[] {"A010106", "A010107"};
-        String[] articlePrices = new String[] {"12.99", "15.99"};
-        String[] articleProductionDates = new String[] {"2014-01-30", "2014-02-16"};
+        final String[] articleCodes = new String[] {"AC8528090", "AC8528091"};
+        final String[] articleTypes = new String[] {"Materiale Acquagym", "Materiale Acquagym"};
+        final String[] articlePositions = new String[] {"A010106", "A010107"};
+        final String[] articlePrices = new String[] {"12.99", "15.99"};
+        final String[] articleProductionDates = new String[] {"2014-01-30", "2014-02-16"};
         List<ArticleModel> articles = new ArrayList<>();
 
         for (int i = 0; i < articleCodes.length; i++) {
@@ -38,19 +38,44 @@ class InsertEntranceControllerTest extends GenericControllerTest {
 
         insertEntranceController.insertArticlesAsEntrance(articles);
 
-        ArticleModel article1 = ArticleModel.getInstance().find(articleCodes[0]);
-        ArticleModel article2 = ArticleModel.getInstance().find(articleCodes[1]);
+        final ArticleModel article1 = ArticleModel.getInstance().find(articleCodes[0]);
+        final ArticleModel article2 = ArticleModel.getInstance().find(articleCodes[1]);
 
         assertNotNull(article1);
         assertNotNull(article2);
         assertEquals(article1.getPosition().getCode(), articlePositions[0]);
         assertEquals(article2.getPosition().getCode(), articlePositions[1]);
 
-        EntranceModel entrance = EntranceModel.getInstance().find(EntranceModel.getInstance().getGreatestCode());
+        final EntranceModel entrance = EntranceModel.getInstance().find(EntranceModel.getInstance().getGreatestCode());
 
         assertEquals(EntranceModel.getInstance().getGreatestCode(), 2);
         assertNotNull(entrance);
         assertEquals(entrance.getCode(), 2);
+    }
 
+    @Test
+    void checkEqualLengthsTest() {
+        final InsertEntranceController insertEntranceController = new InsertEntranceController();
+        final int[] lengthsValid = new int[] { 5, 5, 5, 5 };
+        final int[] lengthsInvalid = new int[] { 1, 2, 3, 4 };
+
+        assertTrue(insertEntranceController.checkEqualLengths(lengthsValid));
+        assertFalse(insertEntranceController.checkEqualLengths(lengthsInvalid));
+    }
+
+    @Test
+    void checkIsAlreadyStoredArticleCodeTest() {
+        final InsertEntranceController insertEntranceController = new InsertEntranceController();
+        final String[] articleCodesValid = new String[] {
+                "AC8388175",
+                "AC8402405"
+        };
+        final String[] articleCodesInvalid = new String[] {
+                "AC000000",
+                "AC999999"
+        };
+
+        assertFalse(insertEntranceController.checkIsAlreadyStoredArticleCodes(articleCodesValid));
+        assertTrue(insertEntranceController.checkIsAlreadyStoredArticleCodes(articleCodesInvalid));
     }
 }
