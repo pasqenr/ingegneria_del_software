cdd9a1919fb82e0285402e980d72b7ef9fe94dae
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-13 14:40:48 +0100

    Implement findAll(), toString() and equals()

--- src/main/java/model/ArticleLeaveModel.java
+++ src/main/java/model/ArticleLeaveModel.java
@@ -3,7 +3,9 @@ package model;
 import database.DatabaseWrapper;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,27 +13,55 @@ import java.util.List;
  */
 public class ArticleLeaveModel extends Model implements GenericDAO {
     private List<ArticleModel> articles;
-    private int orderNumber;
+    private int leaveNumber;
 
     /**
      * Create a new ArticleLeave.
      *
      * @param articles A list of Article.
-     * @param orderNumber A valid order number.
+     * @param leaveNumber A valid order number.
      */
-    public ArticleLeaveModel(List<ArticleModel> articles, int orderNumber) {
+    public ArticleLeaveModel(List<ArticleModel> articles, int leaveNumber) {
         this.articles = articles;
-        this.orderNumber = orderNumber;
+        this.leaveNumber = leaveNumber;
+    }
+
+    public static ArticleLeaveModel getInstance() {
+        return new ArticleLeaveModel(null, 0);
     }
 
     @Override
-    public Model find(String id) {
+    public ArticleLeaveModel find(String id) {
         return null;
     }
 
     @Override
-    public List<Model> findAll() {
-        return null;
+    public List<ArticleLeaveModel> findAll() {
+        List<ArticleLeaveModel> list = new ArrayList<>();
+        DatabaseWrapper db = new DatabaseWrapper();
+        String query =
+                "SELECT ua.numero_bolla, GROUP_CONCAT(ua.codice_articolo) AS codice_articoli " +
+                "FROM uscita_articolo ua " +
+                "GROUP BY ua.numero_bolla";
+        PreparedStatement stmt;
+
+        try {
+            stmt = db.getCon().prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                int leaveNumber = rs.getInt("numero_bolla");
+                String articleCodes = rs.getString("codice_articoli");
+                List<ArticleModel> articles = articleCodesToList(articleCodes);
+                list.add(new ArticleLeaveModel(articles, leaveNumber));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        db.close();
+
+        return list;
     }
 
     @Override
@@ -44,7 +74,7 @@ public class ArticleLeaveModel extends Model implements GenericDAO {
             try {
                 stmt = db.getCon().prepareStatement(query);
                 stmt.setString(1, article.getCode());
-                stmt.setInt(2, orderNumber);
+                stmt.setInt(2, leaveNumber);
                 stmt.execute();
                 db.getCon().commit();
             } catch (SQLException e) {
@@ -55,5 +85,49 @@ public class ArticleLeaveModel extends Model implements GenericDAO {
         db.close();
 
         return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
+
+        stringBuilder.append("leaveNumber: ");
+        stringBuilder.append(leaveNumber);
+        stringBuilder.append(", articles: ");
+        articles.forEach(stringBuilder::append);
+
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (! (o instanceof  ArticleLeaveModel)) {
+            return false;
+        }
+
+        ArticleLeaveModel other = (ArticleLeaveModel)o;
+
+        return leaveNumber == other.leaveNumber &&
+                articles.equals(other.articles);
+    }
+
+    /**
+     * Create a list of ArticleModel from a String of article codes separated by comma ','.
+     *
+     * @param articleCodes Article codes separated by comma ','.
+     * @return A list of ArticleModels.
+     */
+    private List<ArticleModel> articleCodesToList(String articleCodes) {
+        List<ArticleModel> articles = new ArrayList<>();
+        final String[] array = articleCodes.split(",");
+
+        for (String s : array) {
+            articles.add(ArticleModel.getInstance().find(s));
+        }
+
+        return articles;
     }
 }
