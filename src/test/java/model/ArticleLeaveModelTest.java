ad0f8cce7c621d7e587609b27c18aa470e282ffd
Enrico Pasquali <calcifer.code@gmail.com>
2019-02-13 15:28:56 +0100

    Add toStringTest()

--- src/test/java/model/ArticleLeaveModelTest.java
+++ src/test/java/model/ArticleLeaveModelTest.java
@@ -8,6 +8,18 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArticleLeaveModelTest extends GenericModelTest {
+    private final List<ArticleLeaveModel> articleLeaveModelListValid;
+
+    ArticleLeaveModelTest() {
+        int leaveNumber = 1;
+        List<ArticleModel> articles = new ArrayList<>();
+        articles.add(ArticleModel.getInstance().find("AC8402392"));
+        articles.add(ArticleModel.getInstance().find("AC8342928"));
+        ArticleLeaveModel articleLeaveModel = new ArticleLeaveModel(articles, leaveNumber);
+        articleLeaveModelListValid = new ArrayList<>();
+        articleLeaveModelListValid.add(articleLeaveModel);
+    }
+
     @Test
     void findTest() {
         assertNull(ArticleLeaveModel.getInstance().find("000000"));
@@ -15,14 +27,11 @@ class ArticleLeaveModelTest extends GenericModelTest {
 
     @Test
     void findAllTest() {
-        final int leaveNumber = 1;
-        final List<ArticleModel> articles = new ArrayList<>();
-        articles.add(ArticleModel.getInstance().find("AC8402392"));
-        articles.add(ArticleModel.getInstance().find("AC8342928"));
-        final ArticleLeaveModel articleLeaveModel = new ArticleLeaveModel(articles, leaveNumber);
-        final List<ArticleLeaveModel> articleLeaveModelListValid = new ArrayList<>();
-        articleLeaveModelListValid.add(articleLeaveModel);
-
         assertEquals(articleLeaveModelListValid, ArticleLeaveModel.getInstance().findAll());
+    }
+
+    @Test
+    void toStringTest() {
+        assertEquals(articleLeaveModelListValid.toString(), ArticleLeaveModel.getInstance().findAll().toString());
     }
 }
